50a4194f25cd12f6fc1eb8fd972b87ca862c9208
Aidar Putilov <putiloff@gmail.com>
2024-12-26 01:01:50 +0500

    Add lending in books

--- books/serializers.py
+++ books/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
 
-
 from authors.serializers import AuthorSerializer
 from books.models import Book, Lending
 
@@ -17,11 +16,19 @@ class BookSerializer(serializers.ModelSerializer):
 class BookDetailSerializer(serializers.ModelSerializer):
     """Сериализатор для Book RETRIEVE."""
     authors = AuthorSerializer(many=True, read_only=True, source="author")
+    lending = serializers.SerializerMethodField(read_only=True)
+
+    def get_lending(self, book):
+        """Возвращает выдачу книги."""
+        user = self.context["request"].user
+        if Lending.objects.all().filter(user=user).filter(book=book).exists():
+            return user.name
+        return False
 
     class Meta:
         model = Book
         # fields = "__all__"
-        fields = ("id", "title", "description", "authors", "genre", "owner")
+        fields = ("id", "title", "description", "authors", "genre", "owner", "lending")
 
 
 class LendingSerializer(serializers.ModelSerializer):
